9ce7b49db2dc078334d39c8578b064e82090127b
SHALLSHADOWS <96921015+SHALLSHADOWS@users.noreply.github.com>
2023-05-26 18:49:28 +0000

    Add files via upload

---
+++ PizzaPoo/PizzaPoo/Pizza.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPoo
+{
+   public class Pizza
+    {
+        private string nom;
+        private int prix;
+        private string[] ingredients;
+       
+        public Pizza(string nom, int prix, string[] ingredients)
+        {
+            this.nom = nom;
+            this.prix = prix;
+
+            this.ingredients = ingredients;
+
+
+        }
+
+        public string Afficher()
+        {
+            string ingr = (ingredients != null) ? string.Join(", ", ingredients) : string.Empty;
+            return $"Pizza: {this.nom} -- Prix: {this.prix} euro -- Ingrédients: {ingr}";
+
+        }
+
+        public int GetPrix()
+        {
+            return this.prix;
+        }
+
+        public string GetNom()
+        {
+            return this.nom;
+        }
+
+        }
+
+    }
+
---
+++ PizzaPoo/PizzaPoo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using PizzaPoo;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+      // use that constant if you wanna add a dataset from an Api
+      //
+      // private const string apiUrl = "";
+
+      // this one load default data from a json file
+       private const string url = "https://codeavecjonathan.com/res/pizzas1.json";
+
+        static void Main(string[] args)
+        {
+            string json = "";
+
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    json = webClient.DownloadString(url);
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            List<Pizza> list = JsonConvert.DeserializeObject<List<Pizza>>(json);
+
+            Console.WriteLine("Voici la liste de vos pizzas:\n");
+
+            list.Sort((Pizza x, Pizza y) =>
+            {
+                return x.GetPrix().CompareTo(y.GetPrix());
+            });
+
+            foreach (Pizza pizza in list)
+            {
+                Console.WriteLine(pizza.Afficher());
+            }
+        }
+    }
+}
